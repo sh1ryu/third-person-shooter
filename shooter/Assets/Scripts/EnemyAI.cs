ef881b8785176f4238beab8dbf0f330e2efd81e2
shiryu <621nua@gmail.com>
2024-02-11 12:26:04 +0300

    added a scene in which they learned to control AI mobs and set their path

---
+++ shooter/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public PlayerController player;
+    public List<Transform> PatrolPoints;
+    public float ViewAngle;
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+    private void Start()
+    {
+        InitializeComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    private void InitializeComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        NoticePlayerUpdate();
+        PatrolUpdate();
+        ChaseUpdate();
+    }
+
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed = false;
+
+        if (Vector3.Angle(transform.forward, direction) < ViewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+    }
+
+    private void PatrolUpdate()
+    {
+        if (!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance == 0)
+            {
+                PickNewPatrolPoint();
+            }
+        }
+    }
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = PatrolPoints[Random.Range(0, PatrolPoints.Count)].position;
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+}
