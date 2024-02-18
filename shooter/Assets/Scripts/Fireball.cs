77d5136dd9f27a7164de75ba1f2864d72890d0ff
shiryu <621nua@gmail.com>
2024-02-18 13:42:28 +0300

    adding shooting for the character and fireballs for him

---
+++ shooter/Assets/Scripts/Fireball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float Speed;
+    public float Lifetime;
+
+    private void Start()
+    {
+        Invoke("DestroyFirball", Lifetime);
+    }
+
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DestroyFirball();
+    }
+
+    private void DestroyFirball()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ shooter/Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball FireballPrefab;
+    public Transform FireballSourceTransform;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(FireballPrefab, FireballSourceTransform.position, FireballSourceTransform.rotation);
+        }
+    }
+}
